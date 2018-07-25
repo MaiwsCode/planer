76d18e3a06807e1a82a00bc02668acc556717778
MaiwsCode <39537180+MaiwsCode@users.noreply.github.com>
2018-07-25 16:53:57 +0200

    status

--- planerCommon_0.php
+++ planerCommon_0.php
@@ -53,15 +53,6 @@ class planerCommon extends ModuleCommon {
 			$date=null, $dont_notify=false); 
 			}
 		}
-		if ($mode === 'edit'){
-		/*	$records = Utils_RecordBrowserCommon::get_records('Sales_plan',array("date"=> $defaults['date']),array(),array());
-			foreach($records as $record_){
-			Utils_RecordBrowserCommon::update_record('Sales_plan', $record_['id'], array('difficulty_level' => $record['difficulty_level']),$full_update=false, 
-			$date=null, $dont_notify=false); 
-			}*/
-		}
-		if($mode != "display" || $mode != "view"){
-		Base_StatusBarCommon::message($mode);}
 	}
 
 
